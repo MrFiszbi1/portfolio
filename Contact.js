f520621734a821d7ac965096f98ce477ce48bb1c
MrFiszbi <93094151+MrFiszbi@users.noreply.github.com>
2023-03-14 02:28:11 -0700

    Addition of submission to console log and name refactoring
    on click of submit it will now console log the data. refactoring of WorkExperience to resume

--- Contact.js
+++ Contact.js
@@ -11,3 +11,38 @@ $(document).ready(() => {
     },
   });
 });
+
+let submission = document.querySelector("main");
+
+submission.addEventListener("submit", (event) => {
+  const title = "========= Form Submission =========";
+  let username = document.getElementById("name");
+  let name = username.value;
+  let userEmail = document.getElementById("email");
+  let email = userEmail.value;
+  let usereMessage = document.getElementById("message");
+  let message = usereMessage.value;
+
+  if (name == "" && email == "" && message == "") {
+    console.warn("You must enter some data to submit this form");
+  } else {
+    console.group(title);
+    if (name == "") {
+      console.log(`Name: no submission`);
+    } else {
+      console.log(`Name: ${name}`);
+    }
+    if (email == "") {
+      console.log(`Email: no submission`);
+    } else {
+      console.log(`Email: ${email}`);
+    }
+    if (message == "") {
+      console.log(`Message: no submission`);
+    } else {
+      console.log(`Message: ${message}`);
+    }
+    console.groupEnd();
+  }
+  event.preventDefault();
+});
