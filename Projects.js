f140fb1ecd56c7b02cbbe0a3de2a5245b13d3fff
MrFiszbi <93094151+MrFiszbi@users.noreply.github.com>
2023-03-15 09:38:07 -0700

    GitHub link fix and theme work
    fixing of broken link and theme fixes

--- Projects.js
+++ Projects.js
@@ -11,6 +11,14 @@ $(document).ready(() => {
     },
   });
 
+  let portfolioLink = document.createElement("a");
+  portfolioLink.href = `https://github.com/MrFiszbi1/portfolio`;
+  portfolioLink.textContent = `GitHub: https://github.com/MrFiszbi1/portfolio`;
+
+  let ParkLink = document.createElement("a");
+  ParkLink.href = `https://github.com/MrFiszbi1/3D-Amusement-Park`;
+  ParkLink.textContent = `GitHub: https://github.com/MrFiszbi1/3D-Amusement-Park`;
+
   $(`#Projects`)
     .css({
       display: "flex",
@@ -27,9 +35,7 @@ $(document).ready(() => {
             `This is the source code for this portfolio website. The website was the final project in an introductory class on web development as PSU.`
           )
         )
-        .append(
-          ($(`<a>`).url = `Github: https://github.com/MrFiszbi1/portfolio`)
-        )
+        .append(portfolioLink)
     )
     .append(
       $(`<div>`)
@@ -44,11 +50,7 @@ $(document).ready(() => {
             `This was the second project from my computer graphics class at PSU (CS 447). This project consisted of creating a 3D amusement park using OpenGL. The 3D assets were designed by me using Blender. This program was created using FLTK. In order to compile and run this program you will need to have FLTK set up`
           )
         )
-        .append(
-          ($(
-            `<a>`
-          ).url = `Github: https://github.com/MrFiszbi1/3D-Amusement-Park`)
-        )
+        .append(ParkLink)
     );
 
   $(`#carouselControl`).css({
